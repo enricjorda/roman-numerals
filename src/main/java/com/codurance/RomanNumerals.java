520e0253ede9b8d73ddfb6f656684df59395cc9f
enricjorda <enric.jorda@codurance.com>
2019-08-01 11:13:03 +0200

    converts 3

--- src/main/java/com/codurance/RomanNumerals.java
+++ src/main/java/com/codurance/RomanNumerals.java
@@ -4,6 +4,7 @@ public class RomanNumerals {
 
     public static String convert(int decimal) {
 
+        if(decimal == 3) return "III";
         if(decimal == 2) return "II";
         return "I";
     }
--- src/test/java/com/codurance/RomanNumeralsShould.java
+++ src/test/java/com/codurance/RomanNumeralsShould.java
@@ -1,19 +1,26 @@
 package com.codurance;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class RomanNumeralsShould {
 
     @Test
     public void convert_1_to_I() {
 
-        Assert.assertEquals("I", RomanNumerals.convert(1));
+        assertEquals("I", RomanNumerals.convert(1));
     }
 
     @Test
     public void convert_2_to_II() {
 
-        Assert.assertEquals("II", RomanNumerals.convert(2));
+        assertEquals("II", RomanNumerals.convert(2));
+    }
+
+    @Test
+    public void convert_3_to_III() {
+
+        assertEquals("III", RomanNumerals.convert(3));
     }
 }
