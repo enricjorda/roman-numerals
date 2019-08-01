293050b565f91ff5d0a7b3f05e114f17e670a021
enricjorda <enric.jorda@codurance.com>
2019-08-01 16:04:24 +0200

    refactor to enum all

--- src/main/java/com/codurance/RomanNumerals.java
+++ src/main/java/com/codurance/RomanNumerals.java
@@ -4,6 +4,14 @@ public class RomanNumerals {
 
     private static String roman;
     private enum Arabic2Roman{
+        M(1000),
+        D(500),
+        CD(400),
+        C(100),
+        L(50),
+        XL(40),
+        X(10),
+        V(5),
         IV(4),
         I(1);
 
@@ -19,14 +27,6 @@ public class RomanNumerals {
 
     public static String convert(int decimal) {
         roman = "";
-
-        decimal = romanGenerator(decimal, 1000, "M");
-        decimal = romanGenerator(decimal, 400, "CD");
-        decimal = romanGenerator(decimal, 100, "C");
-        decimal = romanGenerator(decimal, 50, "L");
-        decimal = romanGenerator(decimal, 40, "XL");
-        decimal = romanGenerator(decimal, 10, "X");
-        decimal = romanGenerator(decimal, 5, "V");
 
         for (Arabic2Roman number : Arabic2Roman.values()) {
             decimal = romanGenerator(decimal, number.getArabic(), number.toString());
