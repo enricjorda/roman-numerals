1703ac25b67cb2ce45bde5efdb3b9684a20dea2c
Josep Mir <josep.mir@codurance.com>
2019-08-01 15:19:52 +0200

    convert 50 test pass

--- src/main/java/com/codurance/RomanNumerals.java
+++ src/main/java/com/codurance/RomanNumerals.java
@@ -10,6 +10,7 @@ public class RomanNumerals {
         decimal = romanGenerator(decimal, 1000, "M");
         decimal = romanGenerator(decimal, 400, "CD");
         decimal = romanGenerator(decimal, 100, "C");
+        decimal = romanGenerator(decimal, 50, "L");
         decimal = romanGenerator(decimal, 40, "XL");
         decimal = romanGenerator(decimal, 10, "X");
         decimal = romanGenerator(decimal, 5, "V");
